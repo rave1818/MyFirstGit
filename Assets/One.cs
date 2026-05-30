3af7d96c2ee746c2d2135b59d6933f29c4e7812d
rave1818 <fkqpakzm22@gmail.com>
2026-05-30 14:26:59 +0900

    Update One.cs

--- Assets/One.cs
+++ Assets/One.cs
@@ -4,8 +4,9 @@ public class One : MonoBehaviour
 {
     void Start()
     {
-        // 안녕
-        Debug.Log("Hello!");
+        // 안녕123123
+        int damage = 10;
+        Debug.Log("데미지 : " + damage);
     }
 
     void Update()
