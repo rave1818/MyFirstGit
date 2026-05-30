9cf231cd982368628f7dadcfec852b5e862bfcd1
rave1818 <fkqpakzm22@gmail.com>
2026-05-30 14:26:22 +0900

    Update One.cs

--- Assets/One.cs
+++ Assets/One.cs
@@ -4,8 +4,9 @@ public class One : MonoBehaviour
 {
     void Start()
     {
-        // 안녕
-        Debug.Log("Hello!");
+        // 안녕456456
+        float speed = 10.0f;
+        Debug.Log("speed");
     }
 
     void Update()
